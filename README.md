30816437a27a84fb3ed1250e627db987b20e9918
Yojhan <yojhanr95@gmail.com>
2019-09-04 14:09:49 -0500

    First commit Create server to listen basic routes: / and /api/games Add db.sql script

---
+++ README.md
@@ -0,0 +1,11 @@
+# YoutubeFazt-ngCRUD-Node
+
+https://www.youtube.com/watch?v=lxYB79ANJM8&t=1s
+
+# Routes
+
+GET/games: [{}]
+GET/games/$id: {}
+POST/games:
+PUT/games:
+DELETE/games:
---
+++ database/db.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE ng_game_db;
+USE ng_games_db;
+
+CREATE TABLE games(
+    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(180),
+    description VARCHAR(255),
+    urlImg VARCHAR(200),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+DESCRIBE games;
